ebd79a716ba9c8ec68a576ebcbdf0c4872fe7648
runi95 <runar.serigstad@gmail.com>
2019-07-31 21:25:55 +0200

    Made the Fireball spell feel less delayed

--- app/src/Spells/Fireball.ts
+++ app/src/Spells/Fireball.ts
@@ -17,22 +17,18 @@ export class Fireball extends Spell {
         const trig: unit = GetTriggerUnit();
         const trigX: number = GetUnitX(trig);
         const trigY: number = GetUnitY(trig);
+        const angle: number = GetUnitFacing(trig);
         const x: number = GetSpellTargetX();
         const y: number = GetSpellTargetY();
+        const spawnX: number = trigX + 150 * CosBJ(angle);
+        const spawnY: number = trigY + 150 * SinBJ(angle);
         const intelligence: number = GetHeroInt(trig, true);
         const abilityLevel: number = GetUnitAbilityLevel(trig, this.abilityId);
         const damage: number = 100.00 * abilityLevel + 2.00 * intelligence;
-        const distance: number = Math.sqrt(Pow(trigX - x, 2.00) + Pow(trigY - y, 2.00));
-        const travelTime: number = distance / 500.00;
-        const dummy: unit = CreateUnit(GetOwningPlayer(trig), this.dummyUnitId, trigX, trigY, GetUnitFacing(trig));
+        const distance: number = Math.sqrt(Pow(spawnX - x, 2.00) + Pow(spawnY - y, 2.00));
         const loc: location = GetSpellTargetLoc();
-
-        IssuePointOrder(dummy, 'move', x, y);
-
-        const t: Timer = this.timerUtils.NewTimer();
-        t.start(0.05 + travelTime, false, () => {
+        if (distance < 150) {
             DestroyEffect(AddSpecialEffectLocBJ(loc, 'Abilities\\Spells\\Other\\Doom\\DoomDeath.mdl'));
-            KillUnit(dummy);
             const grp: group = GetUnitsInRangeOfLocAll(1000.00, loc);
 
             ForGroup(grp, () => {
@@ -43,8 +39,29 @@ export class Fireball extends Spell {
 
             RemoveLocation(loc);
             DestroyGroup(grp);
+        } else {
+            const dummy: unit = CreateUnit(GetOwningPlayer(trig), this.dummyUnitId, spawnX, spawnY, angle);
 
-            this.timerUtils.ReleaseTimer(t);
-        });
+            IssuePointOrder(dummy, 'move', x, y);
+
+            const travelTime: number = distance / 500.00;
+            const t: Timer = this.timerUtils.NewTimer();
+            t.start(0.25 + travelTime, false, () => {
+                DestroyEffect(AddSpecialEffectLocBJ(loc, 'Abilities\\Spells\\Other\\Doom\\DoomDeath.mdl'));
+                RemoveUnit(dummy);
+                const grp: group = GetUnitsInRangeOfLocAll(1000.00, loc);
+
+                ForGroup(grp, () => {
+                    if (IsUnitEnemy(GetEnumUnit(), GetOwningPlayer(trig))) {
+                        UnitDamageTargetBJ(trig, GetEnumUnit(), damage, ATTACK_TYPE_NORMAL, DAMAGE_TYPE_NORMAL);
+                    }
+                });
+
+                RemoveLocation(loc);
+                DestroyGroup(grp);
+
+                this.timerUtils.ReleaseTimer(t);
+            });
+        }
     }
 }
