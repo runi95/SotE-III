8e1bc11c5ee8cfafd47228955e8fe86342108b3a
runi95 <runar.serigstad@gmail.com>
2019-08-01 18:46:55 +0200

    Changed text colors

--- app/src/Game/Game.ts
+++ app/src/Game/Game.ts
@@ -141,9 +141,9 @@ export class Game {
     }
 
     private printGameModeInfo(): void {
-        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, `|c00FF0000${GetPlayerName(Player(0))}|r may change the game rules`);
-        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, 'Lives: |cffffcc0010|r');
-        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, 'Teams: |cffffcc00none|r');
-        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, 'Sudden death: |cffffcc00true|r');
+        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, `|c00FF0303${GetPlayerName(Player(0))}|r may change the game rules`);
+        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, 'Lives: |cFFFFCC0010|r');
+        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, 'Teams: |cFFFFCC00none|r');
+        DisplayTimedTextToForce(bj_FORCE_ALL_PLAYERS, 30, 'Sudden death: |cFFFFCC00true|r');
     }
 }
