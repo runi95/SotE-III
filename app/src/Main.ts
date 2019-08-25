e619570fef2c2d96cd1621e21956f85978790a2d
runi95 <thegreatandpowerfulmetal@gmail.com>
2019-08-25 12:16:48 +0200

    Replaced team selection frame with radio buttons

--- app/src/Main.ts
+++ app/src/Main.ts
@@ -153,6 +153,61 @@ class FakeCheckbox {
     }
 }
 
+class RadioButton {
+    private readonly hiddenButton: framehandle;
+    private readonly border: framehandle;
+    private readonly check: framehandle;
+    private isChecked: boolean = false;
+    private size: number = 0.015;
+
+    constructor(parent: framehandle, isChecked: boolean = false, size: number = 0.015) {
+        this.isChecked = isChecked;
+        this.size = size;
+        this.hiddenButton = BlzCreateFrameByType('BUTTON', 'radioButtonHiddenButton', parent, 'EscMenuControlBackdropTemplate', 0);
+        this.border = BlzCreateFrameByType('BACKDROP', 'radioButtonBorder', this.hiddenButton, 'ButtonBackdropTemplate', 0);
+        this.check = BlzCreateFrameByType('BACKDROP', 'radioButtonCheck', this.hiddenButton, 'ButtonBackdropTemplate', 0);
+
+        BlzFrameSetTexture(this.border, 'UI\\Widgets\\EscMenu\\Human\\Radiobutton-Background.blp', 0, true);
+        BlzFrameSetTexture(this.check, 'UI\\Widgets\\EscMenu\\Human\\Radiobutton-Button.blp', 0, true);
+
+        BlzFrameSetPoint(this.check, FRAMEPOINT_CENTER, this.hiddenButton, FRAMEPOINT_CENTER, 0.0, 0.0);
+        BlzFrameSetPoint(this.border, FRAMEPOINT_CENTER, this.hiddenButton, FRAMEPOINT_CENTER, 0.0, 0.0);
+
+        this.setChecked(this.isChecked);
+        this.setSize(this.size);
+    }
+
+    public setSize(size: number): void {
+        this.size = size;
+        BlzFrameSetSize(this.hiddenButton, size, size);
+        BlzFrameSetSize(this.border, size, size);
+        BlzFrameSetSize(this.check, size, size);
+    }
+
+    public setChecked(state: boolean): void {
+        this.isChecked = state;
+        BlzFrameSetVisible(this.check, this.isChecked);
+    }
+
+    public getChecked(): boolean {
+        return this.isChecked;
+    }
+
+    public setFramePoint(point: framepointtype, relative: framehandle, relativePoint: framepointtype, x: number, y: number): void {
+        BlzFrameSetPoint(this.hiddenButton, point, relative, relativePoint, x, y);
+    }
+
+    public getHiddenButtonFrame(): framehandle {
+        return this.hiddenButton;
+    }
+
+    public setClickEvent(event: () => void): void {
+        const trig: Trigger = new Trigger();
+        trig.addAction(() => event());
+        trig.registerFrameEvent(this.hiddenButton, FRAMEEVENT_CONTROL_CLICK);
+    }
+}
+
 function createSliderTrigger(frame: framehandle, event: (value: number) => void): void {
     const syncTrig: Trigger = new Trigger();
     syncTrig.addAction(() => {
@@ -205,8 +260,11 @@ function initializeGameOptionFrames(gameGlobals: GameGlobals, randomNumberGenera
     const menuTitle: framehandle = BlzCreateFrame('StandardTitleTextTemplate', menu, 0, 0);
     const fogOfWarCheckbox: framehandle = BlzCreateFrame('QuestCheckBox', menu, 0, 0);
     const fogOfWarText: framehandle = BlzCreateFrame('StandardInfoTextTemplate', fogOfWarCheckbox, 0, 0);
-    const allVersusAllCheckbox: framehandle = BlzCreateFrame('QuestCheckBox', fogOfWarCheckbox, 0, 0);
-    const allVersusAllText: framehandle = BlzCreateFrame('StandardInfoTextTemplate', fogOfWarCheckbox, 0, 0);
+    const teamsText: framehandle = BlzCreateFrame('StandardInfoTextTemplate', fogOfWarCheckbox, 0, 0);
+    const threeVersusThreeRadioButton: RadioButton = new RadioButton(fogOfWarCheckbox, true);
+    const threeVersusThreeText: framehandle = BlzCreateFrame('StandardInfoTextTemplate', fogOfWarCheckbox, 0, 0);
+    const noTeamsRadioButton: RadioButton = new RadioButton(fogOfWarCheckbox, false);
+    const noTeamsText: framehandle = BlzCreateFrame('StandardInfoTextTemplate', fogOfWarCheckbox, 0, 0);
     const suddenDeathCheckbox: framehandle = BlzCreateFrame('QuestCheckBox', fogOfWarCheckbox, 0, 0);
     const suddenDeathText: framehandle = BlzCreateFrame('StandardInfoTextTemplate', fogOfWarCheckbox, 0, 0);
     const livesLabel: framehandle = BlzCreateFrame('StandardInfoTextTemplate', fogOfWarCheckbox, 0, 0);
@@ -219,7 +277,6 @@ function initializeGameOptionFrames(gameGlobals: GameGlobals, randomNumberGenera
     BlzFrameSetSize(menu, 0.3, 0.3);
     BlzFrameSetSize(menuBackdrop, 0.3, 0.3);
     BlzFrameSetSize(fogOfWarCheckbox, 0.02, 0.02);
-    BlzFrameSetSize(allVersusAllCheckbox, 0.02, 0.02);
     BlzFrameSetSize(suddenDeathCheckbox, 0.02, 0.02);
     BlzFrameSetSize(livesSlider, 0.2, 0.02);
     BlzFrameSetSize(startButton, 0.22, 0.035);
@@ -230,9 +287,13 @@ function initializeGameOptionFrames(gameGlobals: GameGlobals, randomNumberGenera
     BlzFrameSetPoint(menuTitle, FRAMEPOINT_TOPLEFT, menu, FRAMEPOINT_TOPLEFT, 0.11, -0.02);
     BlzFrameSetPoint(fogOfWarCheckbox, FRAMEPOINT_CENTER, menuTitle, FRAMEPOINT_CENTER, -0.12, -0.03);
     BlzFrameSetPoint(fogOfWarText, FRAMEPOINT_LEFT, fogOfWarCheckbox, FRAMEPOINT_RIGHT, 0.01, 0.0);
-    BlzFrameSetPoint(allVersusAllCheckbox, FRAMEPOINT_CENTER, fogOfWarCheckbox, FRAMEPOINT_CENTER, 0.0, -0.025);
-    BlzFrameSetPoint(allVersusAllText, FRAMEPOINT_LEFT, allVersusAllCheckbox, FRAMEPOINT_RIGHT, 0.01, 0.0);
-    BlzFrameSetPoint(suddenDeathCheckbox, FRAMEPOINT_CENTER, allVersusAllCheckbox, FRAMEPOINT_CENTER, 0.0, -0.025);
+    BlzFrameSetPoint(teamsText, FRAMEPOINT_LEFT, fogOfWarCheckbox, FRAMEPOINT_LEFT, 0.0, -0.025);
+    threeVersusThreeRadioButton.setFramePoint(FRAMEPOINT_LEFT, teamsText, FRAMEPOINT_RIGHT, 0.01, 0.0);
+    BlzFrameSetPoint(threeVersusThreeText, FRAMEPOINT_LEFT,
+                     threeVersusThreeRadioButton.getHiddenButtonFrame(), FRAMEPOINT_RIGHT, 0.005, 0.0);
+    noTeamsRadioButton.setFramePoint(FRAMEPOINT_LEFT, threeVersusThreeText, FRAMEPOINT_RIGHT, 0.01, 0.0);
+    BlzFrameSetPoint(noTeamsText, FRAMEPOINT_LEFT, noTeamsRadioButton.getHiddenButtonFrame(), FRAMEPOINT_RIGHT, 0.005, 0.0);
+    BlzFrameSetPoint(suddenDeathCheckbox, FRAMEPOINT_LEFT, teamsText, FRAMEPOINT_LEFT, 0.0, -0.025);
     BlzFrameSetPoint(suddenDeathText, FRAMEPOINT_LEFT, suddenDeathCheckbox, FRAMEPOINT_RIGHT, 0.01, 0.0);
     BlzFrameSetPoint(livesMinValueText, FRAMEPOINT_CENTER, suddenDeathCheckbox, FRAMEPOINT_CENTER, 0.0, -0.04);
     BlzFrameSetPoint(livesSlider, FRAMEPOINT_LEFT, livesMinValueText, FRAMEPOINT_RIGHT, 0.01, 0.0);
@@ -245,7 +306,9 @@ function initializeGameOptionFrames(gameGlobals: GameGlobals, randomNumberGenera
 
     BlzFrameSetText(menuTitle, 'SotE Rules');
     BlzFrameSetText(fogOfWarText, 'Disable Fog of War');
-    BlzFrameSetText(allVersusAllText, 'All vs All');
+    BlzFrameSetText(teamsText, 'Teams:');
+    BlzFrameSetText(threeVersusThreeText, '3 vs 3');
+    BlzFrameSetText(noTeamsText, 'All vs All');
     BlzFrameSetText(suddenDeathText, 'Disable Sudden Death');
     BlzFrameSetText(livesLabel, 'Lives');
     BlzFrameSetText(livesMinValueText, '1');
@@ -255,9 +318,7 @@ function initializeGameOptionFrames(gameGlobals: GameGlobals, randomNumberGenera
 
     const fakeMenuFogOfWarCheckbox: FakeCheckbox = new FakeCheckbox(menu);
     const fakeMenuFogOfWarText: framehandle = BlzCreateFrame('StandardValueTextTemplate', fakeMenuFogOfWarCheckbox.getBorderFrame(), 0, 0);
-    const fakeMenuAllVersusAllCheckbox: FakeCheckbox = new FakeCheckbox(fakeMenuFogOfWarCheckbox.getBorderFrame());
-    const fakeMenuAllVersusAllText: framehandle = BlzCreateFrame('StandardValueTextTemplate',
-                                                                 fakeMenuFogOfWarCheckbox.getBorderFrame(), 0, 0);
+    const fakeMenuTeamsText: framehandle = BlzCreateFrame('StandardValueTextTemplate', fakeMenuFogOfWarCheckbox.getBorderFrame(), 0, 0);
     const fakeMenuSuddenDeathCheckbox: FakeCheckbox = new FakeCheckbox(fakeMenuFogOfWarCheckbox.getBorderFrame());
     const fakeMenuSuddenDeathText: framehandle = BlzCreateFrame('StandardValueTextTemplate',
                                                                 fakeMenuFogOfWarCheckbox.getBorderFrame(), 0, 0);
@@ -268,21 +329,18 @@ function initializeGameOptionFrames(gameGlobals: GameGlobals, randomNumberGenera
 
     fakeMenuFogOfWarCheckbox.setFramePoint(FRAMEPOINT_CENTER, menuTitle, FRAMEPOINT_CENTER, -0.12, -0.03);
     BlzFrameSetPoint(fakeMenuFogOfWarText, FRAMEPOINT_LEFT, fakeMenuFogOfWarCheckbox.getBorderFrame(), FRAMEPOINT_RIGHT, 0.01, 0.0);
-    fakeMenuAllVersusAllCheckbox.setFramePoint(FRAMEPOINT_CENTER, fakeMenuFogOfWarCheckbox.getBorderFrame(),
-                                               FRAMEPOINT_CENTER, 0.0, -0.025);
-    BlzFrameSetPoint(fakeMenuAllVersusAllText, FRAMEPOINT_LEFT, fakeMenuAllVersusAllCheckbox.getBorderFrame(), FRAMEPOINT_RIGHT, 0.01, 0.0);
-    fakeMenuSuddenDeathCheckbox.setFramePoint(FRAMEPOINT_CENTER, fakeMenuAllVersusAllCheckbox.getBorderFrame(),
-                                              FRAMEPOINT_CENTER, 0.0, -0.025);
+    BlzFrameSetPoint(fakeMenuTeamsText, FRAMEPOINT_LEFT, fakeMenuFogOfWarCheckbox.getBorderFrame(), FRAMEPOINT_LEFT, 0.0, -0.025);
+    fakeMenuSuddenDeathCheckbox.setFramePoint(FRAMEPOINT_LEFT, fakeMenuTeamsText,
+                                              FRAMEPOINT_LEFT, 0.0, -0.025);
     BlzFrameSetPoint(fakeMenuSuddenDeathText, FRAMEPOINT_LEFT, fakeMenuSuddenDeathCheckbox.getBorderFrame(), FRAMEPOINT_RIGHT, 0.01, 0.0);
     BlzFrameSetPoint(fakeMenuLivesText, FRAMEPOINT_LEFT, fakeMenuSuddenDeathCheckbox.getBorderFrame(), FRAMEPOINT_CENTER, 0.0, -0.025);
     BlzFrameSetPoint(fakeMenuLivesValue, FRAMEPOINT_LEFT, fakeMenuLivesText, FRAMEPOINT_RIGHT, 0.01, 0.0);
     BlzFrameSetPoint(fakeMenuWaitingForHostText, FRAMEPOINT_CENTER, menu, FRAMEPOINT_CENTER, 0.0, -0.11);
     fakeMenuFogOfWarCheckbox.setChecked(false);
-    fakeMenuAllVersusAllCheckbox.setChecked(false);
     fakeMenuSuddenDeathCheckbox.setChecked(false);
 
     BlzFrameSetText(fakeMenuFogOfWarText, 'Disable Fog of War');
-    BlzFrameSetText(fakeMenuAllVersusAllText, 'All vs All');
+    BlzFrameSetText(fakeMenuTeamsText, 'Teams: 3 vs 3');
     BlzFrameSetText(fakeMenuSuddenDeathText, 'Disable Sudden Death');
     BlzFrameSetText(fakeMenuLivesText, 'Lives:');
     BlzFrameSetText(fakeMenuLivesValue, '10');
@@ -295,9 +353,18 @@ function initializeGameOptionFrames(gameGlobals: GameGlobals, randomNumberGenera
     });
 
     let isTeamsEnabled: boolean = true;
-    createCheckboxTrigger(allVersusAllCheckbox, (state: boolean) => {
-        isTeamsEnabled = !state;
-        fakeMenuAllVersusAllCheckbox.setChecked(state);
+    threeVersusThreeRadioButton.setClickEvent(() => {
+        isTeamsEnabled = true;
+        noTeamsRadioButton.setChecked(false);
+        threeVersusThreeRadioButton.setChecked(true);
+        BlzFrameSetText(fakeMenuTeamsText, 'Teams: 3 vs 3');
+    });
+
+    noTeamsRadioButton.setClickEvent(() => {
+        isTeamsEnabled = false;
+        threeVersusThreeRadioButton.setChecked(false);
+        noTeamsRadioButton.setChecked(true);
+        BlzFrameSetText(fakeMenuTeamsText, 'Teams: None');
     });
 
     let isSuddenDeathEnabled: boolean = true;
