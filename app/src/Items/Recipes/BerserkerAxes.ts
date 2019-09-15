f56d1b889b363c0f30c073ab57f75b4030857bbf
runi95 <thegreatandpowerfulmetal@gmail.com>
2019-09-15 13:16:53 +0200

    Added a new recipe BerserkerAxes.ts

---
+++ app/src/Items/Recipes/BerserkerAxes.ts
@@ -0,0 +1,8 @@
+import { ItemRecipe } from '../ItemRecipe';
+
+export class BerserkerAxes extends ItemRecipe {
+    private readonly warAxe: number = FourCC('I00L');
+    protected readonly isRecipeUniquesOnly: boolean = false;
+    protected readonly recipe: number[] = [this.warAxe, this.warAxe];
+    protected readonly resultingItem: number = FourCC('I023');
+}
