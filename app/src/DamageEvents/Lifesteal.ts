e587e7131a7072d4b71f1947d71ad25402ee2cbc
runi95 <thegreatandpowerfulmetal@gmail.com>
2019-09-23 20:25:09 +0200

    Lifesteal now only works for physical damage instead of spell damage

--- app/src/DamageEvents/Lifesteal.ts
+++ app/src/DamageEvents/Lifesteal.ts
@@ -10,7 +10,7 @@ export class Lifesteal implements DamageEvent {
     }
 
     public event(globals: DamageEngineGlobals): void {
-        if (!globals.IsDamageSpell) {
+        if (globals.IsDamageSpell) {
             return;
         }
 
