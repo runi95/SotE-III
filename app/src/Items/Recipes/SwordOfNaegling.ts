d7b328446d5c89417ee3545a9178fe14d36f8a98
runi95 <thegreatandpowerfulmetal@gmail.com>
2019-09-28 16:50:16 +0200

    Created a new recipe SwordOfNaegling.ts

---
+++ app/src/Items/Recipes/SwordOfNaegling.ts
@@ -0,0 +1,8 @@
+import { ItemRecipe } from '../ItemRecipe';
+
+export class SwordOfNaegling extends ItemRecipe {
+    private readonly fragarach: number = FourCC('I017');
+    private readonly swordOfFreyr: number = FourCC('I018');
+    protected readonly recipe: number[] = [this.fragarach, this.swordOfFreyr];
+    protected readonly resultingItem: number = FourCC('I02G');
+}
