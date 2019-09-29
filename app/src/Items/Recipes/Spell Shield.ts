59cf3768cf92aed79447933b09da7e65ab66b438
runi95 <thegreatandpowerfulmetal@gmail.com>
2019-09-29 14:38:48 +0200

    Fixed a bug with the Spell Shield recipe

--- app/src/Items/Recipes/Spell Shield.ts
+++ app/src/Items/Recipes/Spell Shield.ts
@@ -2,7 +2,7 @@ import { ItemRecipe } from '../ItemRecipe';
 
 export class SpellShield extends ItemRecipe {
     private readonly vialOfMagic: number = FourCC('I001');
-    private readonly enchantedShield: number = FourCC('I02A');
+    private readonly enchantedShield: number = FourCC('I027');
     private readonly lionsRing: number = FourCC('I01L');
     protected readonly recipe: number[] = [this.vialOfMagic, this.enchantedShield, this.lionsRing];
     protected readonly resultingItem: number = FourCC('I02F');
