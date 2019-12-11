e9d4bfdee50d503fd7bcda178bf3146134fb1ba9
runi95 <thegreatandpowerfulmetal@gmail.com>
2019-12-11 21:20:07 +0100

    Cleaned up Main.ts by moving framehandle systems out of it

---
+++ app/src/Items/ItemRecipeController.ts
@@ -0,0 +1,164 @@
+import { ItemRecipe } from './ItemRecipe';
+import { AdeptCrystalBall } from './Recipes/AdeptCrystalBall';
+import { CrystalBall } from './BasicItems/CrystalBall';
+import { SobiMask } from './BasicItems/SobiMask';
+import { AdvancedReinforcedHides } from './Recipes/AdvancedReinforcedHides';
+import { ImprovedMoonArmor } from './Recipes/ImprovedMoonArmor';
+import { ImprovedBalancedShield } from './Recipes/ImprovedBalancedShield';
+import { ReinforcedScales } from './Recipes/ReinforcedScales';
+import { MoonArmor } from './BasicItems/MoonArmor';
+import { BalancedShield } from './Recipes/BalancedShield';
+import { IronShield } from './BasicItems/IronShield';
+import { SteelShield } from './Recipes/SteelShield';
+import { CoralScales } from './Recipes/CoralScales';
+import { ReinforcedHide } from './BasicItems/ReinforcedHide';
+import { StuddedLeatherArmor } from './BasicItems/StuddedLeatherArmor';
+import { AgileSlippers } from './Recipes/AgileSlippers';
+import { BootsOfSpeed } from './BasicItems/BootsOfSpeed';
+import { Ancile } from './Recipes/Ancile';
+import { ManaEgg } from './BasicItems/ManaEgg';
+import { ArmoredBoots } from './Recipes/ArmoredBoots';
+import { BerserkerPotion } from './BasicItems/BerserkerPotion';
+import { BloodiedExecutionersAxe } from './BasicItems/BloodiedExecutionersAxe';
+import { BlueSoulstone } from './BasicItems/BlueSoulstone';
+import { Branch } from './BasicItems/Branch';
+import { Claws } from './BasicItems/Claws';
+import { CloakOfShadows } from './BasicItems/CloakOfShadows';
+import { ClockworkPenguin } from './BasicItems/ClockworkPenguin';
+import { EmptySoulcage } from './BasicItems/EmptySoulcage';
+import { EmptyVial } from './BasicItems/EmptyVial';
+import { Flare } from './BasicItems/Flare';
+import { GoblinBattery } from './BasicItems/GoblinBattery';
+import { GreenSoulstone } from './BasicItems/GreenSoulstone';
+import { IronSword } from './BasicItems/IronSword';
+import { LoadedRifle } from './BasicItems/LoadedRifle';
+import { OrbOfMagic } from './BasicItems/OrbOfMagic';
+import { PurpleSoulstone } from './BasicItems/PurpleSoulstone';
+import { Scepter } from './BasicItems/Scepter';
+import { SpeedPotion } from './BasicItems/SpeedPotion';
+import { WarAxe } from './BasicItems/WarAxe';
+import { BerserkerAxes } from './Recipes/BerserkerAxes';
+import { Caduceus } from './Recipes/Caduceus';
+import { CircesStaff } from './Recipes/CircesStaff';
+import { CloakOfShadowWalk } from './Recipes/CloakOfShadowWalk';
+import { ScrollOfAgility } from './Recipes/ScrollOfAgility';
+import { CreatureClaws } from './Recipes/CreatureClaws';
+import { DragonScales } from './Recipes/DragonScales';
+import { DragonWhelpClaws } from './Recipes/DragonWhelpClaws';
+import { EnchantedShield } from './Recipes/EnchantedShield';
+import { EnhancedBerserkerAxes } from './Recipes/EnhancedBerserkerAxes';
+import { Fragarach } from './Recipes/Fragarach';
+import { ManaBlade } from './Recipes/ManaBlade';
+import { IronClaws } from './Recipes/IronClaws';
+import { LightningBolt } from './Recipes/LightningBolt';
+import { TheAegis } from './Recipes/TheAegis';
+import { LionsRing } from './Recipes/LionsRing';
+import { RunedBracers } from './BasicItems/RunedBracers';
+import { LoadedCannon } from './Recipes/LoadedCannon';
+import { ManaAxe } from './Recipes/ManaAxe';
+import { ManaInfusedMask } from './Recipes/ManaInfusedMask';
+import { MaskOfProficiency } from './Recipes/MaskOfProficiency';
+import { VialOfMagic } from './Recipes/VialOfMagic';
+import { MasterCrystalBall } from './Recipes/MasterCrystalBall';
+import { SnowyOwl } from './Recipes/SnowyOwl';
+import { WandOfShadowsight } from './Recipes/WandOfShadowsight';
+import { GoblinNightScope } from './BasicItems/GoblinNightScope';
+import { Soulcage } from './Recipes/Soulcage';
+import { SpellShield } from './Recipes/SpellShield';
+import { SwordOfFreyr } from './Recipes/SwordOfFreyr';
+import { SwordOfNaegling } from './Recipes/SwordOfNaegling';
+import { ThrowableAxe } from './Recipes/ThrowableAxe';
+
+const crystalBall: CrystalBall = new CrystalBall();
+const sobiMask: SobiMask = new SobiMask();
+const moonArmor: MoonArmor = new MoonArmor();
+const ironShield: IronShield = new IronShield();
+const reinforcedHide: ReinforcedHide = new ReinforcedHide();
+const studdedLeatherArmor: StuddedLeatherArmor = new StuddedLeatherArmor();
+const bootsOfSpeed: BootsOfSpeed = new BootsOfSpeed();
+const manaEgg: ManaEgg = new ManaEgg();
+const berserkerPotion: BerserkerPotion = new BerserkerPotion();
+const bloodiedExecutionersAxe: BloodiedExecutionersAxe = new BloodiedExecutionersAxe();
+const blueSoulstone: BlueSoulstone = new BlueSoulstone();
+const branch: Branch = new Branch();
+const claws: Claws = new Claws();
+const cloakOfShadows: CloakOfShadows = new CloakOfShadows();
+const clockworkPenguin: ClockworkPenguin = new ClockworkPenguin();
+const emptySoulcage: EmptySoulcage = new EmptySoulcage();
+const emptyVial: EmptyVial = new EmptyVial();
+const flare: Flare = new Flare();
+const goblinBattery: GoblinBattery = new GoblinBattery();
+const greenSoulstone: GreenSoulstone = new GreenSoulstone();
+const ironSword: IronSword = new IronSword();
+const loadedRifle: LoadedRifle = new LoadedRifle();
+const orbOfMagic: OrbOfMagic = new OrbOfMagic();
+const purpleSoulstone: PurpleSoulstone = new PurpleSoulstone();
+const scepter: Scepter = new Scepter();
+const speedPotion: SpeedPotion = new SpeedPotion();
+const warAxe: WarAxe = new WarAxe();
+const runedBracers: RunedBracers = new RunedBracers();
+const goblinNightScope: GoblinNightScope = new GoblinNightScope();
+const balancedShield: BalancedShield = new BalancedShield(moonArmor, ironShield);
+const improvedMoonArmor: ImprovedMoonArmor = new ImprovedMoonArmor(moonArmor);
+const improvedBalancedShield: ImprovedBalancedShield = new ImprovedBalancedShield(balancedShield);
+const steelShield: SteelShield = new SteelShield(ironShield);
+const coralScales: CoralScales = new CoralScales(reinforcedHide, studdedLeatherArmor);
+const reinforcedScales: ReinforcedScales = new ReinforcedScales(steelShield, coralScales);
+const agileSlippers: AgileSlippers = new AgileSlippers(bootsOfSpeed);
+const scrollOfAgility: ScrollOfAgility = new ScrollOfAgility(agileSlippers, speedPotion);
+const creatureClaws: CreatureClaws = new CreatureClaws(claws, studdedLeatherArmor);
+const dragonWhelpClaws: DragonWhelpClaws = new DragonWhelpClaws(creatureClaws);
+const berserkerAxes: BerserkerAxes = new BerserkerAxes(warAxe);
+const manaBlade: ManaBlade = new ManaBlade(ironSword, emptyVial);
+const ironClaws: IronClaws = new IronClaws(ironSword, claws);
+const theAegis: TheAegis = new TheAegis(steelShield, scepter);
+const vialOfMagic: VialOfMagic = new VialOfMagic(orbOfMagic, emptyVial);
+const maskOfProficiency: MaskOfProficiency = new MaskOfProficiency(orbOfMagic, sobiMask);
+const adeptCrystalBall: AdeptCrystalBall = new AdeptCrystalBall(crystalBall, sobiMask);
+const wandOfShadowsight: WandOfShadowsight = new WandOfShadowsight(goblinNightScope, flare);
+const enchantedShield: EnchantedShield = new EnchantedShield(orbOfMagic, moonArmor);
+const lionsRing: LionsRing = new LionsRing(runedBracers, improvedMoonArmor);
+const manaAxe: ManaAxe = new ManaAxe(warAxe, emptyVial);
+const fragarach: Fragarach = new Fragarach(manaBlade, ironClaws);
+const swordOfFreyr: SwordOfFreyr = new SwordOfFreyr(manaBlade, manaAxe);
+export default [
+    improvedMoonArmor,
+    improvedBalancedShield,
+    reinforcedScales,
+    balancedShield,
+    steelShield,
+    coralScales,
+    agileSlippers,
+    scrollOfAgility,
+    creatureClaws,
+    dragonWhelpClaws,
+    berserkerAxes,
+    manaBlade,
+    ironClaws,
+    theAegis,
+    vialOfMagic,
+    maskOfProficiency,
+    adeptCrystalBall,
+    wandOfShadowsight,
+    enchantedShield,
+    lionsRing,
+    manaAxe,
+    new AdvancedReinforcedHides(improvedMoonArmor, improvedBalancedShield, reinforcedScales),
+    new AgileSlippers(bootsOfSpeed),
+    new Ancile(manaEgg, ironShield),
+    new ArmoredBoots(bootsOfSpeed, steelShield),
+    new Caduceus(branch, studdedLeatherArmor),
+    new CircesStaff(branch, scepter),
+    new CloakOfShadowWalk(cloakOfShadows, scrollOfAgility),
+    new DragonScales(dragonWhelpClaws, coralScales),
+    new EnhancedBerserkerAxes(berserkerAxes, berserkerPotion),
+    new LightningBolt(goblinBattery, theAegis),
+    new LoadedCannon(loadedRifle, ironSword),
+    new ManaInfusedMask(maskOfProficiency, vialOfMagic),
+    new MasterCrystalBall(adeptCrystalBall, vialOfMagic),
+    new SnowyOwl(clockworkPenguin, wandOfShadowsight),
+    new Soulcage(emptySoulcage, greenSoulstone, blueSoulstone, purpleSoulstone),
+    new SpellShield(vialOfMagic, enchantedShield, lionsRing),
+    new SwordOfNaegling(fragarach, swordOfFreyr),
+    new ThrowableAxe(bloodiedExecutionersAxe, scepter),
+];
---
+++ app/src/Items/RecipeSystem.ts
@@ -0,0 +1,335 @@
+import { GameGlobals } from '../Game/GameGlobals';
+import { Trigger } from '../JassOverrides/Trigger';
+// tslint:disable-next-line: import-name
+import items from './ItemRecipeController';
+import { ItemRecipe } from './ItemRecipe';
+
+interface ItemInSlot {
+    itemId: number;
+    includedInRecipe: boolean;
+}
+
+export class RecipeSystem {
+    private readonly gameGlobals: GameGlobals;
+    private readonly menu: framehandle;
+    private selectedItemFrameIndex: number | undefined;
+    private selectedItemIndex: (number | undefined)[] = [];
+    private itemRecipeResultDescriptionFrame: framehandle;
+    private previousItemWindowMax: number;
+    private itemWindowSize: number;
+    private selectedItemFrame: framehandle | undefined;
+    private itemRecipeFrames: framehandle[] = [];
+    private itemRecipeGreenBorderFrames: framehandle[] = [];
+    private itemRecipeResultUpgradeButton: framehandle;
+    private itemRecipeResultIconFrame: framehandle;
+
+    constructor(gameGlobals: GameGlobals) {
+        this.gameGlobals = gameGlobals;
+
+        const originFrameGameUi: framehandle = BlzGetOriginFrame(ORIGIN_FRAME_GAME_UI, 0);
+        this.menu = BlzCreateFrame('EscMenuPopupMenuTemplate', originFrameGameUi, 0, 0);
+        const menuBackdrop: framehandle = BlzCreateFrame('EscMenuButtonBackdropTemplate', this.menu, 0, 0);
+        const menuTitle: framehandle = BlzCreateFrame('StandardTitleTextTemplate', this.menu, 0, 0);
+        const menuScrollbar: framehandle = BlzCreateFrame('EscMenuSliderTemplate', this.menu, 0, 0);
+        const mainButton: framehandle = BlzCreateFrame('ScoreScreenTabButtonTemplate', originFrameGameUi, 0, 0);
+        const mainButtonBackdrop: framehandle = BlzCreateFrameByType('BACKDROP', 'mainButtonBackdrop', mainButton, '', 0);
+
+        BlzFrameSetSize(this.menu, 0.5, 0.38);
+        BlzFrameSetSize(menuScrollbar, 0.48, 0.02);
+        BlzFrameSetPoint(this.menu, FRAMEPOINT_CENTER, originFrameGameUi, FRAMEPOINT_CENTER, 0.0, 0.06);
+        BlzFrameSetPoint(menuTitle, FRAMEPOINT_TOP, this.menu, FRAMEPOINT_TOP, 0.0, -0.02);
+        BlzFrameSetPoint(menuScrollbar, FRAMEPOINT_BOTTOM, this.menu, FRAMEPOINT_BOTTOM, 0.0, 0.01);
+        BlzFrameSetAllPoints(menuBackdrop, this.menu);
+        BlzFrameSetVisible(this.menu, false);
+        BlzFrameSetSize(mainButton, 0.04, 0.04);
+        BlzFrameSetPoint(mainButton, FRAMEPOINT_BOTTOMLEFT, originFrameGameUi, FRAMEPOINT_BOTTOMLEFT, 0.005, 0.15);
+        BlzFrameSetAllPoints(mainButtonBackdrop, mainButton);
+        BlzFrameSetTexture(mainButtonBackdrop, 'ReplaceableTextures\\CommandButtons\\BTNScroll.blp', 0, true);
+        BlzFrameSetText(menuTitle, 'Recipes');
+        BlzFrameSetVisible(mainButton, false);
+
+        const showMainButton: boolean[] = [];
+        for (let i: number = 0; i < bj_MAX_PLAYERS; i++) {
+            showMainButton.push(false);
+            if (this.gameGlobals.PlayerSpawnRegion.length > i) {
+                const index: number = i;
+                const showMainButtonTrigger: Trigger = new Trigger();
+                showMainButtonTrigger.registerEnterRectSimple(this.gameGlobals.PlayerSpawnRegion[i]);
+                showMainButtonTrigger.addAction(() => {
+                    showMainButton[index] = true;
+                    BlzFrameSetVisible(mainButton, showMainButton[GetPlayerId(GetLocalPlayer())]);
+                });
+
+                const hideMainButtonTrigger: Trigger = new Trigger();
+                hideMainButtonTrigger.registerLeaveRectSimple(this.gameGlobals.PlayerSpawnRegion[i]);
+                hideMainButtonTrigger.addAction(() => {
+                    showMainButton[index] = false;
+                    BlzFrameSetVisible(mainButton, showMainButton[GetPlayerId(GetLocalPlayer())]);
+                });
+            }
+        }
+
+        this.itemWindowSize = Math.min(items.length, 11);
+        this.itemRecipeResultIconFrame = BlzCreateFrameByType('BACKDROP', 'itemRecipeResultIcon', this.menu, '', 0);
+        this.itemRecipeResultDescriptionFrame = BlzCreateFrame('StandardValueTextTemplate', this.menu, 0, 0);
+        this.itemRecipeResultUpgradeButton = BlzCreateFrame('ScriptDialogButton', this.menu, 0, 0);
+        BlzFrameSetSize(this.itemRecipeResultIconFrame, 0.04, 0.04);
+        BlzFrameSetPoint(this.itemRecipeResultIconFrame, FRAMEPOINT_TOPLEFT, this.menu, FRAMEPOINT_TOPLEFT, 0.02, -0.06);
+        BlzFrameSetTexture(this.itemRecipeResultIconFrame, 'war3mapImported\\BTNNoItem.blp', 0, true);
+        BlzFrameSetPoint(this.itemRecipeResultDescriptionFrame, FRAMEPOINT_TOPLEFT, this.menu, FRAMEPOINT_TOPLEFT, 0.07, -0.06);
+        BlzFrameSetTextAlignment(this.itemRecipeResultDescriptionFrame, TEXT_JUSTIFY_LEFT, TEXT_JUSTIFY_TOP);
+        BlzFrameSetSize(this.itemRecipeResultDescriptionFrame, 0.41, 0.24);
+        BlzFrameSetPoint(this.itemRecipeResultUpgradeButton, FRAMEPOINT_TOPLEFT, this.menu, FRAMEPOINT_TOPLEFT, 0.01, -0.17);
+        BlzFrameSetSize(this.itemRecipeResultUpgradeButton, 0.06, 0.03);
+        BlzFrameSetEnable(this.itemRecipeResultUpgradeButton, false);
+
+        for (let i: number = 1; i < 7; i++) {
+            const itemIcon: framehandle = BlzCreateFrameByType('BACKDROP', 'ItemIcon', this.menu, '', 0);
+            BlzFrameSetSize(itemIcon, 0.02, 0.02);
+            const x: number = 0.019 + ((i - 1) % 2) * 0.022;
+            const y: number = -0.085 - 0.02 * ((i % 2) + Math.floor(i / 2));
+            BlzFrameSetPoint(itemIcon, FRAMEPOINT_TOPLEFT, this.menu, FRAMEPOINT_TOPLEFT, x, y);
+            BlzFrameSetTexture(itemIcon, 'war3mapImported\\BTNNoItem.blp', 0, true);
+
+            this.itemRecipeFrames.push(itemIcon);
+        }
+
+        for (let i: number = 1; i < 7; i++) {
+            const itemIcon: framehandle = BlzCreateFrameByType('BACKDROP', 'ItemIcon', this.menu, '', 0);
+            BlzFrameSetSize(itemIcon, 0.02, 0.02);
+            const x: number = 0.019 + ((i - 1) % 2) * 0.022;
+            const y: number = -0.085 - 0.02 * ((i % 2) + Math.floor(i / 2));
+            BlzFrameSetPoint(itemIcon, FRAMEPOINT_TOPLEFT, this.menu, FRAMEPOINT_TOPLEFT, x, y);
+            BlzFrameSetTexture(itemIcon, 'war3mapImported\\BTNGreyedItem.blp', 0, true);
+
+            this.itemRecipeGreenBorderFrames.push(itemIcon);
+        }
+
+        for (let i: number = 0; i < bj_MAX_PLAYERS; i++) {
+            this.selectedItemIndex[i] = undefined;
+        }
+
+        this.previousItemWindowMax = this.itemWindowSize - 1;
+
+        const showMainFrame: boolean[] = [];
+        for (let i: number = 0; i < bj_MAX_PLAYERS; i++) {
+            showMainFrame.push(false);
+            if (GetPlayerController(Player(i)) === MAP_CONTROL_USER && GetPlayerSlotState(Player(i)) === PLAYER_SLOT_STATE_PLAYING) {
+                const index: number = i;
+                const t: Trigger = new Trigger();
+                t.registerFrameEvent(mainButton, FRAMEEVENT_CONTROL_CLICK);
+                t.addAction(() => {
+                    showMainFrame[index] = !showMainFrame[index];
+                    BlzFrameSetVisible(this.menu, showMainFrame[GetPlayerId(GetLocalPlayer())]);
+
+                    if (showMainFrame[index]) {
+                        this.unselectItemEvent();
+                    }
+
+                    BlzFrameSetEnable(mainButton, false);
+                    BlzFrameSetEnable(mainButton, true);
+                });
+            }
+        }
+
+        const upgradeButtonTrigger: Trigger = new Trigger();
+        upgradeButtonTrigger.registerFrameEvent(this.itemRecipeResultUpgradeButton, FRAMEEVENT_CONTROL_CLICK);
+        upgradeButtonTrigger.addAction(() => {
+            const triggerPlayerId: number = GetPlayerId(GetTriggerPlayer());
+            const selectedItemForPlayerIndex: number | undefined = this.selectedItemIndex[triggerPlayerId];
+            if (selectedItemForPlayerIndex !== undefined) {
+                const itemsInSlots: ItemInSlot[] = [];
+                for (let i: number = 1; i < 7; i++) {
+                    itemsInSlots.push({
+                        itemId: GetItemTypeId(UnitItemInSlotBJ(gameGlobals.PlayerHero[triggerPlayerId], i)),
+                        includedInRecipe: false,
+                    });
+                }
+
+                let hasAllItems: boolean = true;
+                for (let i: number = 0; i < items[selectedItemForPlayerIndex].recipe.length; i++) {
+                    const foundSlotItem: ItemInSlot | undefined = this.findSlotItem(
+                        itemsInSlots,
+                        items[selectedItemForPlayerIndex].recipe[i].itemId,
+                    );
+
+                    if (foundSlotItem === undefined) {
+                        hasAllItems = false;
+                    }
+                }
+
+                const upgradeGoldCost: number = items[selectedItemForPlayerIndex].goldCost;
+                const playerCurrentGold: number = GetPlayerState(GetTriggerPlayer(), PLAYER_STATE_RESOURCE_GOLD);
+                if (hasAllItems && playerCurrentGold >= upgradeGoldCost) {
+                    SetPlayerState(GetTriggerPlayer(), PLAYER_STATE_RESOURCE_GOLD, playerCurrentGold - upgradeGoldCost);
+
+                    for (let i: number = 0; i < items[selectedItemForPlayerIndex].recipe.length; i++) {
+                        RemoveItem(
+                            GetItemOfTypeFromUnitBJ(
+                                gameGlobals.PlayerHero[triggerPlayerId],
+                                items[selectedItemForPlayerIndex].recipe[i].itemId,
+                            ),
+                        );
+                    }
+                    UnitAddItemById(gameGlobals.PlayerHero[triggerPlayerId], items[selectedItemForPlayerIndex].itemId);
+                    this.selectItemEvent(this.selectedItemFrameIndex as number, triggerPlayerId);
+                }
+            }
+        });
+
+        const itemFrames: framehandle[] = [];
+        for (let i: number = 0; i < this.itemWindowSize && i < items.length; i++) {
+            itemFrames.push(this.createItemFrame(this.menu, items[i].iconPath, i));
+        }
+
+        const itemGoldCost: framehandle[] = [];
+        for (let i: number = 0; i < this.itemWindowSize && i < items.length; i++) {
+            itemGoldCost.push(this.createItemGoldCostFrame(this.menu, items[i].goldCost, i));
+        }
+
+        this.selectedItemFrame = BlzCreateFrameByType('SPRITE', 'selectedItemFrame', this.menu, '', 0);
+        BlzFrameSetVisible(this.selectedItemFrame, false);
+        BlzFrameSetSize(this.selectedItemFrame, 0.04, 0.04);
+        BlzFrameSetPoint(this.selectedItemFrame, FRAMEPOINT_BOTTOMLEFT, this.menu, FRAMEPOINT_BOTTOMLEFT, 0.016, 0.03);
+        BlzFrameSetModel(this.selectedItemFrame, 'UI\\Feedback\\Autocast\\UI-ModalButtonOn.mdx', 0);
+
+        const scrollTrigger: Trigger = new Trigger();
+        scrollTrigger.registerFrameEvent(menuScrollbar, FRAMEEVENT_SLIDER_VALUE_CHANGED);
+        scrollTrigger.addAction(() => {
+            const value: number = BlzFrameGetValue(menuScrollbar);
+            const itemWindowMax: number = this.itemWindowSize + Math.round(value * (items.length - this.itemWindowSize)) - 1;
+            if (itemWindowMax === this.previousItemWindowMax) {
+                return;
+            }
+
+            const itemWindowMaxDifference: number = this.previousItemWindowMax - itemWindowMax;
+            this.previousItemWindowMax = itemWindowMax;
+            const itemWindowMin: number = itemWindowMax + 1 - this.itemWindowSize;
+            if (this.selectedItemFrameIndex === undefined) {
+                BlzFrameSetVisible(this.selectedItemFrame as framehandle, false);
+            } else {
+                this.selectedItemFrameIndex += itemWindowMaxDifference;
+                if (this.selectedItemFrameIndex < 0 || this.selectedItemFrameIndex > this.itemWindowSize - 1) {
+                    BlzFrameSetVisible(this.selectedItemFrame as framehandle, false);
+                } else {
+                    BlzFrameSetPoint(
+                        this.selectedItemFrame as framehandle,
+                        FRAMEPOINT_BOTTOMLEFT,
+                        this.menu,
+                        FRAMEPOINT_BOTTOMLEFT,
+                        0.0175 + 0.0425 * this.selectedItemFrameIndex,
+                        0.03,
+                    );
+                    BlzFrameSetVisible(this.selectedItemFrame as framehandle, true);
+                }
+            }
+
+            for (let i: number = 0; i < itemFrames.length && itemWindowMin + i < items.length; i++) {
+                BlzFrameSetTexture(itemFrames[i], items[itemWindowMin + i].iconPath, 0, true);
+            }
+        });
+    }
+
+    private createItemFrame(parent: framehandle, texture: string, index: number): framehandle {
+        const itemIcon: framehandle = BlzCreateFrameByType('BACKDROP', 'ItemIcon', parent, '', 0);
+        const itemClickFrame: framehandle = BlzCreateFrameByType('BUTTON', 'itemClickFrame', itemIcon, '', 0);
+        BlzFrameSetSize(itemIcon, 0.04, 0.04);
+        BlzFrameSetPoint(itemIcon, FRAMEPOINT_BOTTOMLEFT, parent, FRAMEPOINT_BOTTOMLEFT, 0.0175 + 0.0425 * index, 0.03);
+        BlzFrameSetAllPoints(itemClickFrame, itemIcon);
+        BlzFrameSetTexture(itemIcon, texture, 0, true);
+
+        const t: Trigger = new Trigger();
+        t.registerFrameEvent(itemClickFrame, FRAMEEVENT_CONTROL_CLICK);
+        t.addAction(() => this.selectItemEvent(index, GetPlayerId(GetTriggerPlayer())));
+
+        return itemIcon;
+    }
+
+    private createItemGoldCostFrame(parent: framehandle, goldCost: number, index: number): framehandle {
+        const itemGoldText: framehandle = BlzCreateFrameByType('TEXT', 'itemGoldText', parent, '', 0);
+        BlzFrameSetPoint(itemGoldText, FRAMEPOINT_BOTTOM, parent, FRAMEPOINT_BOTTOMLEFT, 0.04 + 0.0425 * index, 0.07);
+        BlzFrameSetText(itemGoldText, `|cFFFFCC00${goldCost}|r`);
+
+        return itemGoldText;
+    }
+
+    private findSlotItem(itemSlotArray: ItemInSlot[], itemId: number): ItemInSlot | undefined {
+        for (let i: number = 0; i < itemSlotArray.length; i++) {
+            if (!itemSlotArray[i].includedInRecipe && itemSlotArray[i].itemId === itemId) {
+                return itemSlotArray[i];
+            }
+        }
+
+        return undefined;
+    }
+
+    private selectItemEvent(index: number, triggerPlayerId: number): void {
+        this.selectedItemFrameIndex = index;
+        this.selectedItemIndex[triggerPlayerId] = this.selectedItemFrameIndex + this.previousItemWindowMax + 1 - this.itemWindowSize;
+        const item: ItemRecipe = items[this.selectedItemIndex[triggerPlayerId] as number];
+        BlzFrameSetText(this.itemRecipeResultDescriptionFrame, item.description);
+        let hasAllItems: boolean = true;
+        const itemsInSlots: { itemId: number; includedInRecipe: boolean }[] = [];
+        const playerId: number = GetPlayerId(GetLocalPlayer());
+        for (let i: number = 1; i < 7; i++) {
+            itemsInSlots.push({
+                itemId: GetItemTypeId(UnitItemInSlotBJ(this.gameGlobals.PlayerHero[playerId], i)),
+                includedInRecipe: false,
+            });
+        }
+
+        for (let i: number = 0; i < this.itemRecipeFrames.length; i++) {
+            if (i < item.recipe.length) {
+                const foundSlotItem: ItemInSlot | undefined = this.findSlotItem(
+                    itemsInSlots,
+                    items[this.selectedItemIndex[triggerPlayerId] as number].recipe[i].itemId,
+                );
+                if (foundSlotItem !== undefined) {
+                    foundSlotItem.includedInRecipe = true;
+                    BlzFrameSetTexture(this.itemRecipeGreenBorderFrames[i], 'war3mapImported\\BTNGreenBorder.blp', 0, true);
+                } else {
+                    hasAllItems = false;
+                    BlzFrameSetTexture(this.itemRecipeGreenBorderFrames[i], 'war3mapImported\\BTNGreyedItem.blp', 0, true);
+                }
+
+                BlzFrameSetVisible(this.itemRecipeGreenBorderFrames[i], true);
+                BlzFrameSetTexture(this.itemRecipeFrames[i], item.recipe[i].iconPath, 0, true);
+            } else {
+                BlzFrameSetTexture(this.itemRecipeFrames[i], 'war3mapImported\\BTNNoItem.blp', 0, true);
+            }
+        }
+
+        if (hasAllItems) {
+            BlzFrameSetText(this.itemRecipeResultUpgradeButton, item.goldCost.toString());
+            BlzFrameSetEnable(this.itemRecipeResultUpgradeButton, true);
+        } else {
+            BlzFrameSetText(this.itemRecipeResultUpgradeButton, '');
+            BlzFrameSetEnable(this.itemRecipeResultUpgradeButton, false);
+        }
+
+        BlzFrameSetTexture(this.itemRecipeResultIconFrame, item.iconPath, 0, true);
+        BlzFrameSetPoint(
+            this.selectedItemFrame as framehandle,
+            FRAMEPOINT_BOTTOMLEFT,
+            this.menu,
+            FRAMEPOINT_BOTTOMLEFT,
+            0.0175 + 0.0425 * this.selectedItemFrameIndex,
+            0.03,
+        );
+        BlzFrameSetVisible(this.selectedItemFrame as framehandle, true);
+    }
+
+    private unselectItemEvent(): void {
+        for (let i: number = 0; i < this.itemRecipeFrames.length; i++) {
+            BlzFrameSetTexture(this.itemRecipeGreenBorderFrames[i], 'war3mapImported\\BTNGreyedItem.blp', 0, true);
+            BlzFrameSetTexture(this.itemRecipeFrames[i], 'war3mapImported\\BTNNoItem.blp', 0, true);
+            BlzFrameSetText(this.itemRecipeResultUpgradeButton, '');
+            BlzFrameSetEnable(this.itemRecipeResultUpgradeButton, false);
+        }
+
+        BlzFrameSetText(this.itemRecipeResultDescriptionFrame, '');
+        BlzFrameSetTexture(this.itemRecipeResultIconFrame, 'war3mapImported\\BTNNoItem.blp', 0, true);
+        BlzFrameSetVisible(this.selectedItemFrame as framehandle, false);
+        this.selectedItemFrameIndex = undefined;
+    }
+}
