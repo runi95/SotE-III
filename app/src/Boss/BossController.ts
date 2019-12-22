85adb5cbde3d9e08485b3ba9125a0df9b7b1ed33
runi95 <runar.serigstad@gmail.com>
2019-12-22 17:42:34 +0100

    Reworked the Ice Spider boss a little bit to make it more interesting to fight

--- app/src/Boss/BossController.ts
+++ app/src/Boss/BossController.ts
@@ -6,11 +6,12 @@ import { DoomGuard } from './Medium/DoomGuard';
 import { Necromancer } from './Easy/Necromancer';
 import { Hydra } from './Hard/Hydra';
 import { IceSpider } from './Easy/IceSpider';
+import { RandomNumberGenerator } from '../Utility/RandomNumberGenerator';
 
 export class BossController {
     private readonly bosses: Boss[];
 
-    constructor(timerUtils: TimerUtils) {
-        this.bosses = [new DruidOfTheClaw(), new RedDragon(), new DoomGuard(timerUtils), new Necromancer(), new Hydra(), new IceSpider()];
+    constructor(timerUtils: TimerUtils, randomNumberGenerator: RandomNumberGenerator) {
+        this.bosses = [new DruidOfTheClaw(), new RedDragon(), new DoomGuard(timerUtils), new Necromancer(), new Hydra(), new IceSpider(timerUtils, randomNumberGenerator)];
     }
 }
--- app/src/Boss/Easy/IceSpider.ts
+++ app/src/Boss/Easy/IceSpider.ts
@@ -1,4 +1,7 @@
 import { Boss } from '../Boss';
+import { TimerUtils } from '../../Utility/TimerUtils';
+import { Timer } from '../../JassOverrides/Timer';
+import { RandomNumberGenerator } from '../../Utility/RandomNumberGenerator';
 
 export class IceSpider extends Boss {
     protected readonly bossId: number = FourCC('n02C');
@@ -6,8 +9,83 @@ export class IceSpider extends Boss {
     protected readonly y: number = 579.0;
     protected readonly angle: number = 280.0;
     protected readonly lootItemId: number = FourCC('I00C'); // Purple Soulstone
+    private readonly timerUtils: TimerUtils;
+    private readonly randomNumberGenerator: RandomNumberGenerator;
+    private timer: Timer | undefined;
+    private iceSpider: unit | undefined;
+    private readonly eggSackId: number = FourCC('e003');
+    private readonly eggSacks: unit[] = [];
+    private readonly spiderlingId: number = FourCC('u006');
 
-    constructor() {
+    constructor(timerUtils: TimerUtils, randomNumberGenerator: RandomNumberGenerator) {
         super(Rect(14208, 384, 14336, 512));
+
+        this.timerUtils = timerUtils;
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    protected spawnCondition(): boolean {
+        if (!IsUnitType(GetTriggerUnit(), UNIT_TYPE_HERO)) {
+            return false;
+        }
+
+        if (this.eggSacks.length > 0) {
+            return false;
+        }
+
+        return this.iceSpider === undefined;
+    }
+
+    protected spawnAction(): void {
+        this.iceSpider = CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), this.bossId, this.x, this.y, this.angle);
+
+        this.timer = this.timerUtils.newTimer();
+        this.timer.start(1, true, () => {
+            if (this.iceSpider && GetUnitManaPercent(this.iceSpider) === 100) {
+                SetUnitManaPercentBJ(this.iceSpider, 0);
+                this.eggSacks.push(CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), this.eggSackId,
+                    GetUnitX(this.iceSpider) + this.randomNumberGenerator.random(0, 200) - 100,
+                    GetUnitY(this.iceSpider) + this.randomNumberGenerator.random(0, 200) - 100,
+                    0));
+            }
+
+            for (let i: number = 0; i < this.eggSacks.length; i++) {
+                const currentMana: number = GetUnitState(this.eggSacks[i], UNIT_STATE_MANA) - 1;
+                if (currentMana > 0) {
+                    SetUnitManaBJ(this.eggSacks[i], currentMana);
+                } else {
+                    const x: number = GetUnitX(this.eggSacks[i]);
+                    const y: number = GetUnitY(this.eggSacks[i]);
+                    RemoveUnit(this.eggSacks[i]);
+                    this.eggSacks.splice(i, 1);
+
+                    CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), this.spiderlingId, x, y, 0);
+                    CreateUnit(Player(PLAYER_NEUTRAL_AGGRESSIVE), this.spiderlingId, x, y, 0);
+                }
+            }
+        });
+    }
+
+    protected deathCondition(): boolean {
+        return GetUnitTypeId(GetTriggerUnit()) === this.eggSackId || GetTriggerUnit() === this.iceSpider;
+    }
+
+    protected deathAction(): void {
+        if (GetTriggerUnit() === this.iceSpider) {
+            this.iceSpider = undefined;
+
+            if (this.lootItemId === undefined) {
+                return;
+            }
+
+            CreateItem(this.lootItemId, GetUnitX(GetDyingUnit()), GetUnitY(GetDyingUnit()));
+        }
+
+        if (this.iceSpider === undefined && this.eggSacks.length === 0) {
+            if (this.timer !== undefined) {
+                this.timerUtils.releaseTimer(this.timer);
+                this.timer = undefined;
+            }
+        }
     }
 }
