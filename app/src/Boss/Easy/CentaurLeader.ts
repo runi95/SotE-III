b4416f6de993dd634c8ef5b53ee8a4621152cefd
runi95 <runar.serigstad@gmail.com>
2020-01-24 20:23:12 +0100

    Added a new easy boss Centaur Leader with no item drop

---
+++ app/src/Boss/Easy/CentaurLeader.ts
@@ -0,0 +1,8 @@
+import { Boss } from '../Boss';
+
+export class DruidOfTheClaw extends Boss {
+    protected readonly bossId: number = FourCC('n02D');
+    protected readonly x: number = 14654.0;
+    protected readonly y: number = 14658.0;
+    protected readonly angle: number = 235.0;
+}
