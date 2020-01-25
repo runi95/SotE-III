a69e99542a31d78dfaf6003ff6fda02c598b4367
runi95 <runar.serigstad@gmail.com>
2020-01-25 12:50:56 +0100

    Fixed CentaurLeader boss spawn and added new Banshee boss

--- app/src/Boss/BossController.ts
+++ app/src/Boss/BossController.ts
@@ -1,8 +1,10 @@
 import { Boss } from './Boss';
+import { CentaurLeader } from './Easy/CentaurLeader';
 import { DruidOfTheClaw } from './Easy/DruidOfTheClaw';
 import { RedDragon } from './Medium/RedDragon';
 import { TimerUtils } from '../Utility/TimerUtils';
 import { DoomGuard } from './Medium/DoomGuard';
+import { Banshee } from './Medium/Banshee';
 import { Necromancer } from './Easy/Necromancer';
 import { Hydra } from './Hard/Hydra';
 import { IceSpider } from './Easy/IceSpider';
@@ -12,6 +14,18 @@ export class BossController {
     private readonly bosses: Boss[];
 
     constructor(timerUtils: TimerUtils, randomNumberGenerator: RandomNumberGenerator) {
-        this.bosses = [new DruidOfTheClaw(), new RedDragon(), new DoomGuard(timerUtils), new Necromancer(), new Hydra(), new IceSpider(timerUtils, randomNumberGenerator)];
+        this.bosses = [
+            // Easy
+            new CentaurLeader(),
+            new DruidOfTheClaw(),
+            new IceSpider(timerUtils, randomNumberGenerator),
+            new Necromancer(),
+            // Medium
+            new DoomGuard(timerUtils),
+            new RedDragon(),
+            new Banshee(),
+            // Hard
+            new Hydra(),
+        ];
     }
 }
--- app/src/Boss/Medium/Banshee.ts
+++ app/src/Boss/Medium/Banshee.ts
@@ -1,12 +1,12 @@
 import { Boss } from '../Boss';
 
-export class CentaurLeader extends Boss {
+export class Banshee extends Boss {
     protected readonly bossId: number = FourCC('n02D');
     protected readonly x: number = 14654.0;
     protected readonly y: number = 14658.0;
     protected readonly angle: number = 235.0;
 
     constructor() {
-        super(Rect(14624, 14624, 14688, 14688));
+        super(Rect(7456, 14752, 7584, 14880));
     }
 }
