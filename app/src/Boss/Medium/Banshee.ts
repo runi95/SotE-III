4b2f31d228ded2de87493658d7810a3f5bcdcf7d
runi95 <runar.serigstad@gmail.com>
2020-02-01 00:42:13 +0100

    Fixed Banshee boss spawn

--- app/src/Boss/Medium/Banshee.ts
+++ app/src/Boss/Medium/Banshee.ts
@@ -1,10 +1,10 @@
 import { Boss } from '../Boss';
 
 export class Banshee extends Boss {
-    protected readonly bossId: number = FourCC('n02D');
-    protected readonly x: number = 14654.0;
-    protected readonly y: number = 14658.0;
-    protected readonly angle: number = 235.0;
+    protected readonly bossId: number = FourCC('n02E');
+    protected readonly x: number = 7521.0;
+    protected readonly y: number = 14817.0;
+    protected readonly angle: number = 265.0;
 
     constructor() {
         super(Rect(7456, 14752, 7584, 14880));
