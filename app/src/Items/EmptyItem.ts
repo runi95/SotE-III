c60fa01489691e331134c3317136a2815002fb52
runi95 <runar.serigstad@gmail.com>
2020-02-01 14:12:29 +0100

    Removed unused typescript class

--- app/src/Items/EmptyItem.ts
+++
@@ -1,9 +0,0 @@
-import { Item } from './Item';
-
-export class EmptyItem extends Item {
-    public readonly itemId: number = FourCC('');
-    public readonly name: string = '';
-    public readonly goldCost: number = 0;
-    public readonly iconPath: string = '';
-    public readonly description: string = ``;
-}
