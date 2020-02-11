4d1ad1fc817b9f5e8af089f50b20814c37889458
runi95 <runar.serigstad@gmail.com>
2020-02-11 20:04:03 +0100

    Made Vampire Claws a lot more expensive

--- src/Items/BasicItems/VampireClaws.ts
+++ src/Items/BasicItems/VampireClaws.ts
@@ -5,7 +5,7 @@ export class VampireClaws extends Item {
     public readonly itemId: number = FourCC('I02E');
     public readonly name: string = 'Vampire Claws';
     public readonly labels: ItemLabel[] = [ItemLabel.LIFESTEAL];
-    public readonly goldCost: number = 300;
+    public readonly goldCost: number = 608;
     public readonly iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNGhoulFrenzy.blp';
     public readonly description: string = `Practicing with these will surely make you super fast.
 
