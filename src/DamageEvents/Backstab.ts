d6b3e0a1660fc0b0a90ff81a93ee0b42bac21d10
runi95 <runar.serigstad@gmail.com>
2020-02-22 13:10:24 +0100

    Fixed Backstab as it didn't work at all

--- src/DamageEvents/Backstab.ts
+++ src/DamageEvents/Backstab.ts
@@ -13,9 +13,9 @@ export class Backstab implements DamageEvent {
         const targetAngle: number = GetUnitFacing(globals.DamageEventTarget as unit);
         const angleDiff: number = Math.sqrt(Pow(sourceAngle - targetAngle, 2));
         if (angleDiff <= 35 || angleDiff >= 325) {
-            const dmg: number = globals.DamageEventAmount * 2.0;
+            globals.DamageEventAmount *= 2;
             const txt: texttag = CreateTextTag();
-            SetTextTagText(txt, Math.floor(dmg).toString(), 0.02);
+            SetTextTagText(txt, Math.floor(globals.DamageEventAmount).toString(), 0.02);
             SetTextTagPos(
                 txt,
                 GetUnitX(globals.DamageEventSource as unit),
