9ea7ffe923048276e53b679186765130dc506e4f
runi95 <runar.serigstad@gmail.com>
2020-03-06 20:10:30 +0100

    Increased the price and effects of Mana Stone

--- src/Items/Tier1Items/ManaStone.ts
+++ src/Items/Tier1Items/ManaStone.ts
@@ -5,11 +5,11 @@ import { ItemLabel } from '../ItemLabel';
 const itemId: number = FourCC('I015');
 const name: string = 'Mana Stone';
 const labels: ItemLabel[] = [ItemLabel.MAX_MANA];
-const goldCost: number = 1050;
+const goldCost: number = 2400;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNManaStone.blp';
 const description: string = `A stone surging with magical energy.
 
-|cffffcc00Max Mana:|r +350
+|cffffcc00Max Mana:|r +800
 
 |cFF808080Mana is required when casting most spells.|r`;
 
