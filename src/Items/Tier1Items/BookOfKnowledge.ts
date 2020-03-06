8d305ec5b9e6b81235115d8bf020df1da845c143
runi95 <runar.serigstad@gmail.com>
2020-03-06 18:56:09 +0100

    Increased the price and effects of Book of Knowledge

--- src/Items/Tier1Items/BookOfKnowledge.ts
+++ src/Items/Tier1Items/BookOfKnowledge.ts
@@ -5,11 +5,11 @@ import { OrbOfMagic } from '../BaseItems/OrbOfMagic';
 const itemId: number = FourCC('I034');
 const name: string = 'Book of Knowledge';
 const labels: ItemLabel[] = [ItemLabel.INTELLIGENCE];
-const goldCost: number = 1800;
+const goldCost: number = 3300;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNManual3.blp';
 const description: string = `A book full of information about the strangest things.
 
-|cffffcc00Intelligence:|r +18
+|cffffcc00Intelligence:|r +33
 
 |cFF808080Intelligence increases the damage done by your spells.|r`;
 
