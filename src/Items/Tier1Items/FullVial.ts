8c6c4f42e7881575cd83a667f0a53779b2dc9bee
runi95 <runar.serigstad@gmail.com>
2020-03-06 20:03:00 +0100

    Increased the price and effects of Full Vial

--- src/Items/Tier1Items/FullVial.ts
+++ src/Items/Tier1Items/FullVial.ts
@@ -6,12 +6,12 @@ import { SobiMask } from '../BaseItems/SobiMask';
 const itemId: number = FourCC('I016');
 const name: string = 'Full Vial';
 const labels: ItemLabel[] = [ItemLabel.MAX_MANA, ItemLabel.MANA_REGEN];
-const goldCost: number = 1150;
+const goldCost: number = 2285;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNVialFull.blp';
 const description: string = `A vial full of potency.
 
-|cffffcc00Max Mana:|r +100
-|cffffcc00Mana regen:|r +10
+|cffffcc00Max Mana:|r +450
+|cffffcc00Mana regen:|r +11
 
 |cFF808080Mana is required when casting most spells.|r`;
 
