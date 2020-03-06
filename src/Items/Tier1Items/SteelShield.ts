5b021cf732448f0fbd4de9616ef9cc1b54fcaa26
runi95 <runar.serigstad@gmail.com>
2020-03-06 23:12:56 +0100

    Increased the cost and effects of Steel Shield

--- src/Items/Tier1Items/SteelShield.ts
+++ src/Items/Tier1Items/SteelShield.ts
@@ -5,11 +5,11 @@ import { ItemLabel } from '../ItemLabel';
 const itemId: number = FourCC('I00J');
 const name: string = 'Steel Shield';
 const labels: ItemLabel[] = [ItemLabel.BLOCK];
-const goldCost: number = 536;
+const goldCost: number = 2345;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNHumanArmorUpTwo.blp';
 const description: string = `A weak shield capable of blocking incoming physical damage.
 
-|cffffcc00Block:|r +8
+|cffffcc00Block:|r +35
 
 |cFF808080Incoming physical damage is reduced by the amount of block you have.|r`;
 
