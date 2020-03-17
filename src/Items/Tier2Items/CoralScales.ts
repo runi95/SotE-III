6153db8544f36eae2c65dd5de12943909689176e
runi95 <runar.serigstad@gmail.com>
2020-03-17 18:22:27 +0100

    Increased the cost and effects of Coral Scales

--- src/Items/Tier2Items/CoralScales.ts
+++ src/Items/Tier2Items/CoralScales.ts
@@ -6,12 +6,12 @@ import { ReinforcedLeatherArmor } from '../Tier1Items/ReinforcedLeatherArmor';
 const itemId: number = FourCC('I00N');
 const name: string = 'Coral Scales';
 const labels: ItemLabel[] = [ItemLabel.HEALTH_REGEN, ItemLabel.MAX_HEALTH];
-const goldCost: number = 1100;
+const goldCost: number = 9000;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNNagaArmorUp1.blp';
 const description: string = `Very strong, defensive scales.
 
-|cffffcc00Health regen:|r +10
-|cffffcc00Max health:|r +100
+|cffffcc00Health regen:|r +60
+|cffffcc00Max health:|r +1200
 
 |cFF808080Health regeneration determines how much health you're passively regaining every second.|r`;
 
