5c317272857d7d6fad170d62606a7da36f924462
runi95 <runar.serigstad@gmail.com>
2020-03-17 18:20:13 +0100

    Increased the cost and effects of Fragarach

--- src/Items/Tier2Items/Fragarach.ts
+++ src/Items/Tier2Items/Fragarach.ts
@@ -6,12 +6,12 @@ import { ItemLabel } from '../ItemLabel';
 const itemId: number = FourCC('I017');
 const name: string = 'Fragarach';
 const labels: ItemLabel[] = [ItemLabel.ATTACK_DAMAGE];
-const goldCost: number = 2800;
+const goldCost: number = 8450;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNAirBender.blp';
 const description: string = `A sword so sharp it can cut whispers in half.
 
-|cffffcc00Agility:|r +10
-|cffffcc00Attack damage:|r +30
+|cffffcc00Agility:|r +20
+|cffffcc00Attack damage:|r +125
 |cffffcc00Effect:|r Slows the movement speed of nearby enemy units by 20%
 
 |cFF808080Effects are special properties that usually trigger on an event.|r`;
