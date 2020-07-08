5415bfe4e99b4495fafd2a950024623b0e37c4d0
runi95 <runar.serigstad@gmail.com>
2020-07-08 19:29:17 +0200

    Increased the cost and effects of ImprovedCreatureClaws

--- src/Items/Tier2Items/ImprovedCreatureClaws.ts
+++ src/Items/Tier2Items/ImprovedCreatureClaws.ts
@@ -5,11 +5,11 @@ import { ItemLabel } from '../ItemLabel';
 const itemId: number = FourCC('I025');
 const name: string = 'Improved Creature Claws';
 const labels: ItemLabel[] = [ItemLabel.AGILITY];
-const goldCost: number = 3600;
+const goldCost: number = 7200;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNImprovedCreatureAttack.tga';
 const description: string = `These claws seem to cut through things better than any sharp knife would.
 
-|cffffcc00Agility:|r +40
+|cffffcc00Agility:|r +80
 
 |cFF808080Agility increases your attack and movement speed.|r`;
 
