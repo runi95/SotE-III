1ce2cf27fadf15124a662df6e21322d5bbc5a5ad
runi95 <runar.serigstad@gmail.com>
2020-07-08 19:34:48 +0200

    Increased the cost and effects of StrengthOfTheWild

--- src/Items/Tier2Items/StrengthOfTheWild.ts
+++ src/Items/Tier2Items/StrengthOfTheWild.ts
@@ -5,11 +5,11 @@ import { ItemLabel } from '../ItemLabel';
 const itemId: number = FourCC('I02A');
 const name: string = 'Strength of the Wild';
 const labels: ItemLabel[] = [ItemLabel.STRENGTH];
-const goldCost: number = 3600;
+const goldCost: number = 7200;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNDOCAdeptTraining.blp';
 const description: string = `Only the strong can survive in the wild.
 
-|cffffcc00Strength:|r +30
+|cffffcc00Strength:|r +60
 
 |cFF808080Strength increases your max health and health regen.|r`;
 
