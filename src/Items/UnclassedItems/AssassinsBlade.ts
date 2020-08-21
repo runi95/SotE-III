c0960dabbd7c9403fbdd06092680b2b964665f8e
runi95 <runar.serigstad@gmail.com>
2020-08-21 20:11:33 +0200

    Updated Assassin's Blade

--- src/Items/UnclassedItems/AssassinsBlade.ts
+++ src/Items/UnclassedItems/AssassinsBlade.ts
@@ -6,11 +6,13 @@ const name: string = `Assassin's Blade`;
 const labels: ItemLabel[] = [];
 const goldCost: number = 500;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNDaggerOfEscape.blp';
-const description: string = `The assassin's preferred weapon of choice.
+const description: string = `Pierces through the thickest of defenses.
 
-|cffffcc00Unique:|r Your first attack every 30 seconds deals 300 bonus damage.
+|cffffcc00Attack damage:|r +200
+|cffffcc00Piercing:|r +600
+|cffffcc00Passive:|r Every attack has a 5% chance to completely ignore block
 
-|cFF808080Unique item effects do not stack with itself.|r`;
+|cFF808080Piercing lets a certain amount of damage go through block.|r`;
 
 export class AssassinsBlade extends Item {
     constructor() {
