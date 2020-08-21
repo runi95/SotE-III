d21f2d12cfe135446d50b6dea6cc9378878cd76e
runi95 <runar.serigstad@gmail.com>
2020-08-21 22:55:43 +0200

    Added a new item Moon Blade

---
+++ src/Items/Tier2Items/MoonBlade.ts
@@ -0,0 +1,23 @@
+import { ItemRecipe } from '../ItemRecipe';
+import { ItemLabel } from '../ItemLabel';
+import { ImprovedMoonArmor } from '../Tier1Items/ImprovedMoonArmor';
+import { IronSword } from '../BaseItems/IronSword';
+
+const itemId: number = FourCC('I051');
+const name: string = 'Moon Blade';
+const labels: ItemLabel[] = [ItemLabel.SPLASH, ItemLabel.ATTACK_DAMAGE];
+const goldCost: number = 1725;
+const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNMoonBlade.dds';
+const description: string = `A normal sword infused with a special kind of moon armor.
+
+|cffffcc00Attack Damage:|r +25
+|cffffcc00Resistance:|r +20
+|cffffcc00Unique:|r Physical attacks temporarily increases resistance by +5 (up to 50)
+
+|cFF808080Incoming spell damage is reduced by the amount of resistance you have.|r`;
+
+export class MoonBlade extends ItemRecipe {
+    constructor(improvedMoonArmor: ImprovedMoonArmor, ironSword: IronSword) {
+        super(itemId, name, labels, goldCost, iconPath, description, [improvedMoonArmor, ironSword]);
+    }
+}
