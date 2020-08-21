156ab71429d85460d7eb30b99ac9edaff30000a0
runi95 <runar.serigstad@gmail.com>
2020-08-21 17:27:13 +0200

    Reduced the effects and cost of VampireClaws

--- src/Items/BaseItems/VampireClaws.ts
+++ src/Items/BaseItems/VampireClaws.ts
@@ -4,11 +4,11 @@ import { ItemLabel } from '../ItemLabel';
 const itemId: number = FourCC('I02E');
 const name: string = 'Vampire Claws';
 const labels: ItemLabel[] = [ItemLabel.LIFESTEAL];
-const goldCost: number = 600;
+const goldCost: number = 350;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNGhoulFrenzy.blp';
 const description: string = `Practicing with these will surely make you super fast.
 
-|cffffcc00Lifesteal:|r +24
+|cffffcc00Lifesteal:|r +15
 
 |cFF808080Lifesteal determines the maximum amount of health regained when attacking enemy units.|r`;
 
