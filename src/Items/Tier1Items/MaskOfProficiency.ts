15088d2a03dabde9377fef77c66ee9d0f6378da5
runi95 <runar.serigstad@gmail.com>
2020-08-22 14:14:44 +0200

    Reworked MaskOfProficiency

--- src/Items/Tier1Items/MaskOfProficiency.ts
+++ src/Items/Tier1Items/MaskOfProficiency.ts
@@ -5,13 +5,13 @@ import { ItemLabel } from '../ItemLabel';
 
 const itemId: number = FourCC('I026');
 const name: string = 'Mask of Proficiency';
-const labels: ItemLabel[] = [ItemLabel.INTELLIGENCE, ItemLabel.MANA_REGEN];
-const goldCost: number = 2395;
+const labels: ItemLabel[] = [ItemLabel.MAX_MANA, ItemLabel.MANA_REGEN];
+const goldCost: number = 950;
 const iconPath: string = 'ReplaceableTextures\\CommandButtons\\BTNGuldanSkull.blp';
-const description: string = `A mask that is said to be worn by a master necromancer.
+const description: string = `I wonder what this thing does.
 
-|cffffcc00Intelligence:|r +18
-|cffffcc00Mana regen:|r +7
+|cffffcc00Max Mana:|r +100
+|cffffcc00Mana regen:|r +10
 
 |cFF808080Intelligence increases the damage done by your spells.|r`;
 
