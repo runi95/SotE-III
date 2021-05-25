1420a708b784575f451a646d719589259ccc6639
runi95 <runar.serigstad@gmail.com>
2021-05-25 17:43:44 +0200

    Reduced the cost and effect of MaulOfStrength

--- src/Items/Tier1Items/MaulOfStrength.ts
+++ src/Items/Tier1Items/MaulOfStrength.ts
@@ -6,11 +6,11 @@ import { StuddedLeatherArmor } from '../BaseItems/StuddedLeatherArmor';
 const itemId: number = FourCC('I023');
 const name = 'Maul of Strength';
 const labels: ItemLabel[] = [ItemLabel.STRENGTH, ItemLabel.MAX_HEALTH];
-const goldCost = 1760;
+const goldCost = 1460;
 const iconPath = 'ReplaceableTextures\\CommandButtons\\BTNHammer.blp';
 const description = `A heavy maul only wielded by the strongest of heroes.
 
-|cffffcc00Strength:|r +16
+|cffffcc00Strength:|r +11
 |cffffcc00Max health:|r +200
 |cffffcc00Effect(1):|r Taking damage increases item charges by 1 (max 15 charges) for 5 seconds
 |cffffcc00Effect(2):|r Physical damage dealt is increased by 5 for each item stack
