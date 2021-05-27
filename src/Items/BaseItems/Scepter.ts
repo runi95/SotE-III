9cd7f5d345f894b43634047ac5acca982dbe1d37
runi95 <runar.serigstad@gmail.com>
2021-05-27 22:08:08 +0200

    Buffed the Scepter item by reducing it's cost and increasing the footman summon duration

--- src/Items/BaseItems/Scepter.ts
+++ src/Items/BaseItems/Scepter.ts
@@ -4,13 +4,13 @@ import { ItemLabel } from '../ItemLabel';
 const itemId: number = FourCC('I010');
 const name = 'Scepter';
 const labels: ItemLabel[] = [];
-const goldCost = 400;
+const goldCost = 300;
 const iconPath = 'ReplaceableTextures\\CommandButtons\\BTNPriestAdept.blp';
 const description = `A magical scepter showing high status and wealth.
 
 |cffffcc00Use:|r Spawn 3 Footmen to fight for you
 |cffffcc00Mana Cost:|r 50
-|cffffcc00Duration:|r 20
+|cffffcc00Duration:|r 40
 |cffffcc00Cooldown:|r 60
 
 |cFF808080Use is an effect that occurs when an item is used by clicking on it.|r`;
