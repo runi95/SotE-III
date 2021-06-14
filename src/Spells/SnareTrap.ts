78eddaa95c09d0526abe400b8f770cb547bd14e5
runi95 <runar.serigstad@gmail.com>
2021-06-14 19:33:09 +0200

    Increased the duration of SnareTrap from 60 to 300 seconds

--- src/Spells/SnareTrap.ts
+++ src/Spells/SnareTrap.ts
@@ -6,7 +6,7 @@ export class SnareTrap extends Spell {
     private readonly timedLifeBuffId: number = FourCC('BTLF');
 
     protected action(): void {
-        UnitApplyTimedLifeBJ(60, this.timedLifeBuffId,
+        UnitApplyTimedLifeBJ(300, this.timedLifeBuffId,
                              CreateUnit(GetOwningPlayer(GetTriggerUnit()),
                                         this.dummyUnitId, GetSpellTargetX(), GetSpellTargetY(), bj_UNIT_FACING));
     }
