7d63e2a0894245ac2de90c944c23d859ddb131d5
runi95 <runar.serigstad@gmail.com>
2021-06-19 11:34:17 +0200

    Reduced the price of OrbOfCorruption

--- src/Items/Tier1Items/RingOfKings.ts
+++ src/Items/Tier1Items/RingOfKings.ts
@@ -6,12 +6,12 @@ import { OrbOfCorruption } from '../BaseItems/OrbOfCorruption';
 const itemId: number = FourCC('I055');
 const name = 'Ring of Kings';
 const labels: ItemLabel[] = [ItemLabel.STRENGTH];
-const goldCost = 1150;
+const goldCost = 1000;
 const iconPath = 'ReplaceableTextures\\CommandButtons\\BTNRingSkull.blp';
 const description = `A royal ring fit for a king
 
 |cffffcc00Strength:|r +10
-|cffffcc00Unique:|r Decreases the armor of attacked units by 2
+|cffffcc00Unique:|r Decreases the armor of attacked units by 3
 
 |cFF808080Strength increases your max health and health regen.|r`;
 
