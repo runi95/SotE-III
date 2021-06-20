1f421ed894810a2b2721e2f8e8c5c04b723bcf3c
runi95 <runar.serigstad@gmail.com>
2021-06-20 11:06:31 +0200

    Slightly reworked Venom and added some visible effects to it

--- src/Utility/VenomUtils.ts
+++ src/Utility/VenomUtils.ts
@@ -26,10 +26,33 @@ export class VenomUtils {
             this.envenomedUnits.set(handleId, envenomedUnit);
 
             const t: Timer = this.timerUtils.newTimer();
-            t.start(1, true, () => {
-                SetUnitLifeBJ(envenomedUnit.getUnit(), Math.max(GetUnitState(envenomedUnit.getUnit(), UNIT_STATE_LIFE) - envenomedUnit.getVenom(), 1));
-                envenomedUnit.setVenom(Math.floor(envenomedUnit.getVenom() / 2));
-                if (envenomedUnit.getVenom() < 1) {
+            t.start(1.5, true, () => {
+                let endVenom = false;
+                if (!UnitAlive(envenomedUnit.getUnit()) || envenomedUnit.getVenom() <= 1) {
+                    endVenom = true;
+                } else {
+                    const currentHealth: number = GetUnitState(envenomedUnit.getUnit(), UNIT_STATE_LIFE);
+                    const newHealth: number = Math.max(currentHealth - envenomedUnit.getVenom(), 1);
+                    SetUnitLifeBJ(envenomedUnit.getUnit(), newHealth);
+                    envenomedUnit.setVenom(Math.floor(envenomedUnit.getVenom() / 2));
+                    DestroyEffect(AddSpecialEffectTarget('Abilities\\Spells\\Items\\OrbVenom\\OrbVenomSpecialArt.mdl', envenomedUnit.getUnit(), 'chest'));
+                    const venomDamageTaken: number = Math.floor(currentHealth - newHealth);
+                    const txt: texttag = CreateTextTag();
+                    SetTextTagText(txt, `${venomDamageTaken}`, 0.023);
+                    SetTextTagPos(
+                        txt,
+                        GetUnitX(envenomedUnit.getUnit()),
+                        GetUnitY(envenomedUnit.getUnit()),
+                        BlzGetUnitZ(envenomedUnit.getUnit()),
+                    );
+                    SetTextTagColor(txt, 60.0, 155.0, 50.0, 255.0);
+                    SetTextTagPermanent(txt, false);
+                    SetTextTagLifespan(txt, 4.0);
+                    SetTextTagVelocity(txt, 0, 0.04);
+                    SetTextTagFadepoint(txt, 2.5);
+                }
+
+                if (endVenom) {
                     this.envenomedUnits.delete(handleId);
                     this.timerUtils.releaseTimer(t);
                 }
