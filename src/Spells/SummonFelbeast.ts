c2346bc4cbe2d61e6184c1f360868b5a557f7db3
runi95 <runar.serigstad@gmail.com>
2022-02-26 14:59:04 +0100

    Fixed issues with summoned `Felbeast's` `Shadow Meld` ability

--- src/Spells/SummonFelbeast.ts
+++ src/Spells/SummonFelbeast.ts
@@ -3,7 +3,6 @@ import { Spell } from './Spell';
 export class SummonFelbeast extends Spell {
     protected readonly abilityId: number = FourCC('A013');
     private readonly summonId: number = FourCC('n00F');
-    private readonly shadowMeldAbilityId: number = FourCC('A04G');
     private readonly cleavingAttackAbilityId: number = FourCC('A014');
     private readonly bashAbilityId: number = FourCC('A04F');
     private readonly criticalStrikeAbilityId: number = FourCC('A04E');
@@ -20,7 +19,6 @@ export class SummonFelbeast extends Spell {
         SetUnitLifePercentBJ(summon, 100);
         BlzSetUnitBaseDamage(summon, Math.ceil(3 * abilityLevel + str), 0);
         UnitApplyTimedLifeBJ(60, this.timedLifeBuffId, summon);
-        UnitAddAbility(summon, this.shadowMeldAbilityId);
 
         if (abilityLevel > 2) {
             UnitAddAbility(summon, this.cleavingAttackAbilityId);
